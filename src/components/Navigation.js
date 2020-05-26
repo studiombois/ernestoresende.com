75c50aa4494028506c46ea665c7b18533810c8ec
thisisernesto <ernestoresende@hotmail.com>
2020-05-26 13:53:21 -0300

    added: navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link as GatsbyLink } from 'gatsby';
+
+import theme from '@styles/theme';
+import mixins from '@styles/mixins';
+import media from '@styles/media';
+const { colors, zIndex, fontSizes } = theme;
+
+
+
+export const NameContainer = styled(props => <GatsbyLink {...props} />)`
+  color: ${colors.highlightPink};
+  text-decoration: none;
+
+  ${media.tablet`display:none;`};
+  bold {
+    font-weight: 900;
+  }
+`;
+const Nav = styled.nav`
+  position: fixed;
+  min-width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 85px;
+  background-color: ${colors.mainBackground};
+  z-index: ${zIndex.header};
+  ${mixins.sidePadding}
+`;
+export const NavList = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+export const StyledLink = styled(props => <GatsbyLink {...props} />)`
+  font-size: ${fontSizes.xs};
+  font-weight: 400;
+  margin: 0 0 0 50px;
+  color: ${colors.mainType};
+  display: inline-block;
+  vertical-align: middle;
+  position: relative;
+  overflow: hidden;
+  transition: all ease-in-out 0.2s;
+  
+  :hover {
+    color: ${colors.highlightPink};
+  }
+  ${media.desktop`margin: 0 25px 0 0;`};
+  ${media.tablet`
+    font-size: ${fontSizes.xxs};
+    margin: 0 25px 0 0;
+  `};
+`;
+
+class Navigation extends React.Component {
+  render () {
+    return (
+      <Nav>
+      <NameContainer to="/">ERNESTO<bold>RESENDE</bold></NameContainer>
+      <NavList>
+        <StyledLink to="/portfolio">Portfolio</StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/blog">Blog</StyledLink>
+        <StyledLink to="/contact">Contact</StyledLink>
+      </NavList>
+    </Nav>
+    )
+  };
+};
+
+export default Navigation;
