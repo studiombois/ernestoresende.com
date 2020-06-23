9f8f1df0604de89145d4fd40568be48cdc96a05a
Ernesto Resende <ernestoresende@hotmail.com>
2020-06-23 20:30:14 -0300

    added: small hero component for secondary pages

---
+++ src/components/SmallHero.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import HeroMask from '@icons/HeroMask';
+
+import media from '@styles/media'
+import mixins from '@styles/mixins';
+
+const MainContainer = styled.div `
+  height: 180px;
+  background: var(--color-gradients);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  ${media.thone`height: 150px;`};
+`;
+const HeroWrapper = styled.div `
+  ${mixins.sidePadding}
+  ${mixins.desktopAlignCenter}
+`;
+const HeroMaskWrapper = styled.div `
+  display: block;
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  z-index: 3;
+  bottom: -20px;
+`;
+
+const SmallHero = ({ children }) => {
+  return (
+    <MainContainer>
+        <HeroWrapper>
+          {children}
+        </HeroWrapper>
+        <HeroMaskWrapper>
+          <HeroMask />
+        </HeroMaskWrapper>
+    </MainContainer>
+  )
+}
+
+SmallHero.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default SmallHero;
