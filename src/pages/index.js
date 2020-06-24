09ac17a7defd9329b14646028509863d39e09099
Ernesto Resende <ernestoresende@hotmail.com>
2020-06-24 12:45:42 -0300

    added: contact component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Hero from '@components/Hero'
 import HeroBody from '@components/HeroBody';
 import HomeGrid from '@components/HomeGrid';
 import Navigation from '@components/Navigation';
+import Contact from '@components/Contact';
 import SEO from '@components/Seo';
 
 const IndexPage = () => {
@@ -17,6 +18,7 @@ const IndexPage = () => {
         <HeroBody/>
       </Hero>
       <HomeGrid />
+      <Contact />
     </React.Fragment>
   )
 }
