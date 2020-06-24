f439bacf1efe0d3aced47486cc679bfc2b3c59f0
Ernesto Resende <ernestoresende@hotmail.com>
2020-06-24 12:43:48 -0300

    added: styled link css to mixins

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -33,6 +33,46 @@ const mixins = {
     padding: 0 50px;
     ${media.phablet`padding: 0 25px;`};
   `,
+
+  styledLink: css`
+    color: var(--color-highlights);
+    -webkit-transform: perspective(1px) translateZ(0);
+    transform: perspective(1px) translateZ(0);
+    box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+    position: relative;
+    overflow: hidden;
+    margin-right: 20px;
+    &:before {
+      content: "";
+      position: absolute;
+      left: 51%;
+      right: 51%;
+      bottom: 0;
+      background: var(--color-highlights);
+      height: 2px;
+      -webkit-transition-property: left, right;
+      transition-property: left, right;
+      -webkit-transition-duration: 0.2s;
+      transition-duration: 0.2s;
+      -webkit-transition-timing-function: ease-out;
+      transition-timing-function: ease-out;
+    }
+    &:hover:before {
+      left: 0;
+      right: 0;
+    }
+    &:focus:before {
+      left: 0;
+      right: 0;
+    }
+    &:active:before {
+      left: 0;
+      right: 0;
+    }
+    @media (max-width: 500px) {
+      font-size: 16px;
+    }
+  `
 };
 
 export default mixins;
