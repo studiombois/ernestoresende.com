c17a6c8d52c8c5c35ce8d6054a02c17d90eb7157
Ernesto Resende <ernestoresende@hotmail.com>
2020-07-18 16:58:12 -0300

    changed: navbar behavior on scroll

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,49 +3,53 @@ import styled from 'styled-components';
 import { Link as GatsbyLink } from 'gatsby';
 
 import DarkToggle from '@components/DarkToggle';
-import { ThemeContext } from '@components/ThemeContext';
 
 import theme from '@styles/theme';
 import mixins from '@styles/mixins';
 import media from '@styles/media';
-const { zIndex, fontSizes } = theme;
+const { fontSizes } = theme;
 
 
 const Main = styled.div `
   margin: 0 auto;
+  top: 0;
+  position: sticky;
   background-color: var(--color-navBackground);
+  z-index: 15;
   ${media.tablet`display:none;`}
 `;
 
 const NameContainer = styled(props => <GatsbyLink {...props} />)`
   color: var(--color-highlights);
   text-decoration: none;
-
-  bold {
-    font-weight: 900;
-  }
+  padding-right: 64px;
+  font-size: 20px;
+  letter-spacing: -.1rem;
 `;
 const Nav = styled.nav`
   ${mixins.desktopAlignCenter}
   display: flex;
   justify-content: space-between;
   align-items: center;
-  min-height: 85px;
-  z-index: ${zIndex.header};
+  min-height: 65px;
   ${mixins.sidePadding}
   ${media.tablet`min-height: 50px;`}
 `;
+const LeftBlock = styled.div `
+  display: flex;
+  flex-direction: row;
+`
 const NavList = styled.ul`
   display: flex;
   list-style: none;
 `;
 const StyledLink = styled(props => <GatsbyLink {...props} />)`
-  font-size: ${fontSizes.xs};
-  font-weight: 400;
-  margin: 0 0 0 30px;
+  font-size: 16px;
+  font-weight: 600;
+  margin: 0 0 0 25px;
   color: var(--color-text);
   display: inline-block;
-  vertical-align: middle;
+  top: 2px;
   position: relative;
   overflow: hidden;
   transition: all ease-in-out 0.1s;
@@ -61,23 +65,25 @@ const StyledLink = styled(props => <GatsbyLink {...props} />)`
   `};
 `;
 const Bold = styled.span`
-  font-weight: 800;
+  font-weight: 700;
 `;
 
 class Navigation extends React.Component {
   render () {
     return (
       <Main>
-        <Nav>
-          <NameContainer to="/">ERNESTO<Bold>RESENDE</Bold></NameContainer>
-          <NavList>
-            <StyledLink to="/portfolio">Portfolio</StyledLink>
-            <StyledLink to="/about">About</StyledLink>
-            <StyledLink to="/blog">Blog</StyledLink>
-            <StyledLink to="/contact">Contact</StyledLink>
-            <DarkToggle />
-          </NavList>
-        </Nav>
+          <Nav>
+          <LeftBlock>
+            <NameContainer to="/">ERNESTO<Bold>RESENDE</Bold></NameContainer>
+            <NavList>
+              <StyledLink to="/portfolio">portfolio</StyledLink>
+              <StyledLink to="/about">about</StyledLink>
+              <StyledLink to="/blog">blog</StyledLink>
+              <StyledLink to="/contact">connect</StyledLink>
+            </NavList>
+          </LeftBlock>
+          <DarkToggle />
+          </Nav>
       </Main>
     )
   };
