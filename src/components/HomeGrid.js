8c702b76204cbd89986523acd0c11717a0ed83e8
Ernesto Resende <ernestoresende@hotmail.com>
2020-07-22 21:25:44 -0300

    chore: section cards added on the homepage

--- src/components/HomeGrid.js
+++ src/components/HomeGrid.js
@@ -19,12 +19,40 @@ const Main = styled.div `
 const Recent = styled.h2 `
   font-size: 18px;
   font-weight: 600;
-  padding: 50px 0 30px;
-  color: var(--color-highlights);
+  padding: 30px 0 50px;
+  color: var(--color-sectionTitleHighlights);
 `;
 const Archive = styled(props => <GatsbyLink {...props} />)`
   ${mixins.styledLink}
 `;
+const ContactSection = styled.div `
+  padding-top: 100px;
+`
+const SocialWrapper = styled.div `
+  display: flex;
+  align-items: flex-start;
+`
+const StyledIcon = styled.a`
+  align-items: center;
+  padding: 10px 0 10px 0;
+    border-radius: 2px;
+    display: flex;
+    flex-direction: column;
+    flex-shrink: 1;
+    font-size: 0.875rem;
+    line-height: 1;
+    -webkit-box-pack: center;
+    justify-content: center;
+    position: relative;
+    text-align: center;
+    text-decoration: none;
+    .iconify {
+      color: var(--color-text);
+      width: 6rem;
+      height: 6rem;
+      padding-right: 20px;
+    }
+`
 
 const blogListQuery = graphql `
   query {  
@@ -34,6 +62,7 @@ const blogListQuery = graphql `
     ) {
       edges {
         node {
+          excerpt(pruneLength: 230)
           fields {
             slug
           }
@@ -42,6 +71,7 @@ const blogListQuery = graphql `
             description
             date(fromNow: true)
           }
+          timeToRead
         }
       }
     }
@@ -60,20 +90,46 @@ const HomeGrid = () => {
       <Main>
       <SectionCard
       title="Writing."
-      description="I love to write in detail about the stuff I work on. This is where I'll write about front-end development, design, and general tech related stuff." />
+      description="I love to write in detail about the stuff I work on."
+      descriptionSecondLine="This is where I'll write about front-end development, design, and general tech related stuff."
+      />
         <div>
           <Recent>RECENTLY PUBLISHED</Recent>
-          {list.map(({  node }, i) => (
+          {list.map(({ node }, i) => (
             <BlogItem
               key={i}
               slug={node.fields.slug}
-              date={node.frontmatter.date}
+              timeToRead={node.timeToRead}
               title={node.frontmatter.title}
+              excerpt={node.excerpt}
               description={node.frontmatter.description}
             />
           ))}
           <Archive to="/blog">See all blog posts</Archive>
         </div>
+
+        <ContactSection>
+          <SectionCard
+            title="Hey there!"
+            description="As of now, I'm actually looking for a job opportunity in front-end development and/or UI design roles. If you like what you see here, consider sending me a mail or reaching out in any of my socials:"
+            contactEmail="ernestoresende@hotmail.com">
+
+            <SocialWrapper>
+
+              <StyledIcon href="#">
+                <span class="iconify" data-icon="ant-design:linkedin-filled" data-inline="false"></span>
+              </StyledIcon>
+              <StyledIcon href="#">
+                <span class="iconify" data-icon="ant-design:github-filled" data-inline="false"></span>
+              </StyledIcon>
+              <StyledIcon href="#">
+                <span class="iconify" data-icon="ant-design:twitter-outlined" data-inline="false"></span>
+              </StyledIcon>
+
+            </SocialWrapper>
+
+          </SectionCard>
+        </ContactSection>
       </Main>
   )
 }
