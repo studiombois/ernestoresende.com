e39f3a46132afc1d97578c7107714f311e5158a0
Ernesto Resende <ernestoresende@hotmail.com>
2020-08-04 11:10:56 -0300

    changed: theme toggle layout

--- src/components/DarkToggle.js
+++ src/components/DarkToggle.js
@@ -8,10 +8,23 @@ import media from '@styles/media';
 long time before giving up on doing the animation myself and looking for a nice
 toggle on Codepen, and yours, my friend... Looks fantastic. */
 
+
+const ToggleContainer = styled.div `
+  display: flex;
+  align-items: center;
+`
+const ToggleIcon = styled.div`
+  .iconify {
+      color: var(--color-titleHighlights);
+      width: 3.2rem;
+      height: 3.2rem;
+    }
+`
+
 const ToggleLabel = styled.label`
   display: block;
-  padding-left: 26px;
-  ${media.tablet`padding-left: 0;`};
+  padding-left: 12px;
+  ${media.tablet`padding-left: 12px;`};
 
   .toggle-checkbox {
   position: absolute;
@@ -23,7 +36,7 @@ const ToggleLabel = styled.label`
 
 .toggle-slot {
   position: relative;
-  height: 2.5rem;
+  height: 2.4rem;
   width: 5.2rem;
   border: 1px solid var(--color-toggleBorder);
   border-radius: 5rem;
@@ -46,15 +59,15 @@ const ToggleLabel = styled.label`
 }
 
 .toggle-button {
-  transform: translateX(calc((5.2rem - 100%)));
+  transform: translateX(calc((5.3rem - 100%)));
   position: absolute;
-  height: 2.5rem;
-  width: 2.5rem;
-  top: -1px;
-  
+  height: 3rem;
+  width: 3rem;
+  top: -5px;
+  left: -2px;
   border-radius: 50%;
   background-color: #ffeccf;
-  box-shadow: inset 0px 0px 0px 1em #ffd700;
+  box-shadow: inset 0px 0px 0px 1em rgb(33, 35, 44);
   transition: background-color 250ms, border-color 250ms, transform 500ms cubic-bezier(.26,2,.46,.71);
 }
 
@@ -62,50 +75,6 @@ const ToggleLabel = styled.label`
   background-color: #485367;
   box-shadow: inset 0px 0px 0px 1em white;
   transform: translateX(0px);
-}
-
-{/* Control the icon size trough the icon classe selection and the icon positioning and 
-transition on the wrapper class selection */}
-.sun-icon {
-  position: absolute;
-  height: 18px;
-  width: 18px;
-  color: #ffbb52;
-}
-
-.sun-icon-wrapper {
-  position: absolute;
-  height: 18px;
-  width: 18px;
-  opacity: 1;
-  transform: translate(0.20em, 0.25rem) rotate(15deg);
-  transform-origin: 50% 50%;
-  transition: opacity 150ms, transform 500ms cubic-bezier(.26,2,.46,.71);
-}
-
-.toggle-checkbox:checked ~ .toggle-slot .sun-icon-wrapper {
-  opacity: 0;
-}
-
-.moon-icon {
-  position: absolute;
-  height: 16px;
-  width: 16px;
-  color: white;
-}
-
-.moon-icon-wrapper {
-  position: absolute;
-  height: 16px;
-  width: 16px;
-  opacity: 0;
-  transform: translate(1.7em, 0.30rem) rotate(0deg);
-  transform-origin: 50% 50%;
-  transition: opacity 150ms, transform 500ms cubic-bezier(.26,2.5,.46,.71);
-}
-
-.toggle-checkbox:checked ~ .toggle-slot .moon-icon-wrapper {
-  opacity: 1;
 }
 `
 
@@ -117,28 +86,26 @@ const DarkToggle = () => {
   }
 
   return (
-  <ToggleLabel>
-    <input 
-    className='toggle-checkbox' 
-    type='checkbox'
-    checked={colorMode === 'dark'}
-    onChange={ev => {
-          setColorMode(ev.target.checked ? 'dark' : 'light');
-        }}
-      />{' '}
+    <ToggleContainer>
+      <ToggleIcon>
+        <span class="iconify" data-icon="heroicons-outline:light-bulb" data-inline="false"></span>
+      </ToggleIcon>
 
+      <ToggleLabel>
+        <input 
+        className='toggle-checkbox' 
+        type='checkbox'
+        checked={colorMode === 'dark'}
+        onChange={ev => {
+              setColorMode(ev.target.checked ? 'dark' : 'light');
+            }}
+          />{' '}
 
-    {/* Working on showing the element outline only when tabbed, but no when selected.*/}
-    <div title="Change between Light/Dark Mode" role="button" className='toggle-slot'>
-      <div className='sun-icon-wrapper'>
-        <div className="iconify sun-icon" data-icon="feather-sun" data-inline="false"></div>
-      </div>
-      <div className='toggle-button'></div>
-      <div className='moon-icon-wrapper'>
-        <div className="iconify moon-icon" data-icon="feather-moon" data-inline="false"></div>
-      </div>
-    </div>
-  </ToggleLabel>
+        <div title="Change between Light/Dark Mode" role="button" className='toggle-slot'>
+          <div className='toggle-button'></div>
+        </div>
+      </ToggleLabel>
+    </ToggleContainer>
   );
 };
 
